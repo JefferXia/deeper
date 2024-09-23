7f530e4c7584fdf6035f419d391613b5e3b64ab1
xiazhifeng <zhifeng.xia@caocaoglobal.com>
2024-09-23 23:58:19 +0800

    边栏

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -24,20 +24,20 @@ const Sidebar = ({ children }: { children: React.ReactNode }) => {
     {
       icon: Home,
       href: '/',
-      active: segments.length === 0 || segments.includes('c'),
-      label: 'Home',
+      active: segments.length === 0,
+      label: '首页',
     },
     {
       icon: Flame,
       href: '/hot',
       active: segments.includes('hot') || segments.includes('video-analysis'),
-      label: 'Videos',
+      label: '爆款',
     },
     {
       icon: BookOpenText,
       href: '/library',
-      active: segments.includes('library'),
-      label: 'Library',
+      active: segments.includes('library') || segments.includes('c'),
+      label: '问AI',
     },
   ];
 
@@ -45,28 +45,33 @@ const Sidebar = ({ children }: { children: React.ReactNode }) => {
     <div>
       <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-20 lg:flex-col">
         <div className="flex grow flex-col items-center justify-between gap-y-5 overflow-y-auto bg-light-secondary dark:bg-dark-secondary px-2 py-8">
-          <a className='block p-3 rounded-full bg-[#24A0ED]' href="/">
-            <Clapperboard className="cursor-pointer" />
-          </a>
-          <VerticalIconContainer>
-            {navLinks.map((link, i) => (
-              <Link
-                key={i}
-                href={link.href}
-                className={cn(
-                  'relative flex flex-row items-center justify-center cursor-pointer hover:bg-black/10 dark:hover:bg-white/10 duration-150 transition w-full py-2 rounded-lg',
-                  link.active
-                    ? 'text-black dark:text-white'
-                    : 'text-black/70 dark:text-white/70',
-                )}
-              >
-                <link.icon />
-                {link.active && (
-                  <div className="absolute right-0 -mr-2 h-full w-1 rounded-l-lg bg-black dark:bg-white" />
-                )}
-              </Link>
-            ))}
-          </VerticalIconContainer>
+          <div className=''>
+            <a className='block mb-10 p-3 rounded-full bg-[#24A0ED]' href="/">
+              <Clapperboard className="cursor-pointer" />
+            </a>
+            <VerticalIconContainer>
+              {navLinks.map((link, i) => (
+                <Link
+                  key={i}
+                  href={link.href}
+                  className={cn(
+                    'relative flex flex-col items-center justify-center cursor-pointer hover:bg-black/10 dark:hover:bg-white/10 duration-150 transition w-full py-2 rounded-lg',
+                    link.active
+                      ? 'text-black dark:text-white'
+                      : 'text-black/70 dark:text-white/70',
+                  )}
+                >
+                  <div>
+                    <link.icon />
+                  </div>
+                  <p className='text-xs mt-1'>{link.label}</p>
+                  {link.active && (
+                    <div className="absolute right-0 -mr-2 h-full w-1 rounded-l-lg bg-black dark:bg-white" />
+                  )}
+                </Link>
+              ))}
+            </VerticalIconContainer>
+          </div>
 
           <ThemeSwitcher />
 
